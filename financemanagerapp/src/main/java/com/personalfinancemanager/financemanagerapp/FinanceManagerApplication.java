3d7df58a09c6645e3f04192fe4d80a9009251704
Wirlly Silva <wirlly.silva@gmail.com>
2024-02-06 23:26:30 -0300

    Some packages were added

---
+++ financemanagerapp/src/main/java/com/personalfinancemanager/financemanagerapp/FinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinancemanager.financemanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceManagerApplication.class, args);
+	}
+
+}
