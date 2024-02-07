0d345fbe68d1a02e15e730ee041f8ddf4db040cf
Wirlly Silva <wirlly.silva@gmail.com>
2024-02-07 09:40:50 -0300

    Changes on workspace were made
    I made some configurations to resolve certain issues.

--- financemanagerapp/src/main/java/com/personalfinancemanager/financemanagerapp/FinancemanagerappApplication.java
+++ financemanagerapp/src/main/java/com/personalfinancemanager/financemanagerapp/FinancemanagerappApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceManagerApplication {
+public class FinancemanagerappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinanceManagerApplication.class, args);
+
+		SpringApplication.run(FinancemanagerappApplication.class, args);
 	}
 
 }
---
+++ financemanagerapp/src/main/java/com/personalfinancemanager/financemanagerapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.personalfinancemanager.financemanagerapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Isso vai procurar por um arquivo chamado "index.html" na pasta "resources/templates"
+    }
+}
